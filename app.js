975fceda3a0b672dc25f046462160fc5f308aa88
Lyxuff <45715257+Lyxuff@users.noreply.github.com>
2020-09-30 00:00:32 +0100

    Welcome reminder feature

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ client.once('ready', () => {
     console.log('Ready');
 })
 
-// if a message is sent, creep on non bots
+
+// command based features
 client.on("message", (message) => { 
-    if(message.author.bot || !message.content.startsWith(config.prefix))return;
     
+    // if a message is sent
+    if(message.author.bot || !message.content.startsWith(config.prefix))return;
     const kekw = client.emojis.cache.find(emoji => emoji.name === "kekw");
 
     // Seeks command
@@ -29,6 +31,7 @@ client.on("message", (message) => {
     // Purge Command
     if(command === 'purge') {    
         const amountInt = parseInt(amount[0])
+
         // Command errors
         if(isNaN(amountInt))return message.channel.send(`That's not a vaild number silly! ${kekw}`);
         if(amountInt >= 100)return message.channel.send(`That's too many silly! Try a number below 100... ${kekw}`);
@@ -40,6 +43,25 @@ client.on("message", (message) => {
         });
 
     }
+})
+
+// Automated features
+client.on("guildMemberAdd", member => {
+    
+    //guild variables
+    const guild = member.guild;
+
+    //member variables
+    const owner = guild.owner
+    const kevin = client.users.cache.get('253176948128350208')
+
+    //event variables
+    const event = guild.channels.cache.find(c => c.name.slice(2) === 'event-signup');
+    const vote = guild.channels.cache.get('736625344294944898');
+    const ranks = guild.channels.cache.get('735601656330190908');
+
+    kevin.send("Someone has joined, copy pasta the following and and welcome them!")
+    kevin.send(`Welcome ${member}! As a starter, I suggest you check out our ${event} and ${vote}. If you're into trials, check out ${ranks}. Hope you enjoy your stay here and let us know if you have questions :)`)
 })
 
 // login to Discord with your app's token
