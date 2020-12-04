ee1353cea903c1917812d1df7c73dfeedb7ff98b
Kevin Jansen <kevitius99@gmail.com>
2020-12-04 18:35:57 +0100

    Made the guildMemberAdd event dynamically loaded

--- app.js
+++ app.js
@@ -23,38 +23,20 @@ commandFiles.map(commandFile => {
 });
 console.log('All commands have been loaded')
 
+// Discover what event files exists in the filesystem
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+// Map through the array of filenames
 eventFiles.map(eventFile => {
+    // Require the event from the filesystem
     const event = require(`./events/${eventFile}`);
 
+    // Get the event name & bind the event to a client event
     let eventName = eventFile.split('.')[0]
     console.log(`Watching for ${eventName} event.`)
     client.on(eventName, event.bind(null, client));
 })
 
 console.log('All event are being watched.')
-
-// Automated features
-client.on("guildMemberAdd", member => {
-
-    console.log('New member')
-
-    //guild variables
-    const guild = member.guild;
-
-    //member variables
-    const owner = guild.owner
-    const kevin = client.users.cache.get('253176948128350208')
-
-    // channels
-    const cache = guild.channels.cache;
-    const event = cache.find(c => c.name.slice(2) === 'event-signup');
-    const vote = cache.get('736625344294944898');
-    const ranks = cache.get('735601656330190908');
-
-    kevin.send("Someone has joined, copy pasta the following and and welcome them!")
-    kevin.send(`Welcome ${member}! As a starter, I suggest you check out our ${event} and ${vote}. If you're into trials, check out ${ranks}. Hope you enjoy your stay here and let us know if you have questions :)`)
-})
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
---
+++ events/guildMemberAdd.js
@@ -0,0 +1,19 @@
+module.exports = (client, message) => {
+    console.log('New member')
+
+    //guild variables
+    const guild = member.guild;
+
+    //member variables
+    const owner = guild.owner
+    const kevin = client.users.cache.get('253176948128350208')
+
+    // channels
+    const cache = guild.channels.cache;
+    const event = cache.find(c => c.name.slice(2) === 'event-signup');
+    const vote = cache.get('736625344294944898');
+    const ranks = cache.get('735601656330190908');
+
+    kevin.send("Someone has joined, copy pasta the following and and welcome them!")
+    kevin.send(`Welcome ${member}! As a starter, I suggest you check out our ${event} and ${vote}. If you're into trials, check out ${ranks}. Hope you enjoy your stay here and let us know if you have questions :)`)
+};
