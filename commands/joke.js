3083f8f2bcc30a04d791aaf180911dbb66c4eb1b
Kevin Jansen <kevitius99@gmail.com>
2020-12-07 16:43:58 +0100

    Added Joke command

---
+++ commands/joke.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'joke',
+    description: 'Tells you a funny joke. Usage: `.joke`',
+    guildOnly: true,
+    async execute(client, message, args) {
+        const {setup, punchline} = await fetch('https://official-joke-api.appspot.com/jokes/random')
+            .then(response => response.json())
+            .catch(console.error);
+
+        const embed = new Discord.MessageEmbed();
+        // embed.setAuthor('The Master Joker')
+        embed.setTitle(setup);
+        embed.setDescription(punchline);
+
+        await message.channel.send(embed);
+    }
+}
