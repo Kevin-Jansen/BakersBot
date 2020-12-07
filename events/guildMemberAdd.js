6327fcb34224047fa518eeaba8db165f7e6c15f1
Kevin Jansen <kevitius99@gmail.com>
2020-12-07 19:54:46 +0100

    Dynamic fetching of channels

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -7,14 +7,12 @@ module.exports = (client, member) => {
     //member variables
     const owner = guild.owner
 
-    const kevin = member.guild.members.cache.get('253176948128350208')
-
     // channels
     const cache = guild.channels.cache;
     const event = cache.find(c => c.name.slice(2) === 'event-signup');
-    const vote = cache.get('736625344294944898');
-    const ranks = cache.get('735601656330190908');
+    const vote = cache.find(c => c.name.slice(2) === 'event-votes');
+    const ranks = cache.find(c => c.name.slice(2) === 'trial-ranks');
 
-    kevin.send("Someone has joined, copy pasta the following and and welcome them!")
-    kevin.send(`Welcome ${member}! As a starter, I suggest you check out our ${event} and ${vote}. If you're into trials, check out ${ranks}. Hope you enjoy your stay here and let us know if you have questions :)`)
+    owner.send("Someone has joined, copy pasta the following and and welcome them!")
+    owner.send(`Welcome ${member}! As a starter, I suggest you check out our ${event} and ${vote}. If you're into trials, check out ${ranks}. Hope you enjoy your stay here and let us know if you have questions :)`)
 };
