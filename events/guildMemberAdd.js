f5a039918ef1c364a15bc077239d74116a89a01c
Kyle Orton <kyleorton.oa@gmail.com>
2020-12-18 13:12:57 +0000

    Text change for add member

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,18 +1,23 @@
 module.exports = (client, member) => {
-    console.log('New member')
+	console.log("New member");
 
-    //guild variables
-    const guild = member.guild;
+	//guild variables
+	const guild = member.guild;
 
-    //member variables
-    const owner = guild.owner
+	//member variables
+	const owner = guild.owner;
 
-    // channels
-    const cache = guild.channels.cache;
-    const event = cache.find(c => c.name.slice(2) === 'event-signup');
-    const vote = cache.find(c => c.name.slice(2) === 'event-votes');
-    const ranks = cache.find(c => c.name.slice(2) === 'trial-ranks');
+	// channels
+	const cache = guild.channels.cache;
+	const about = cache.find((c) => c.name.slice(2) === "about-us");
+	const event = cache.find((c) => c.name.slice(2) === "event-signup");
+	const vote = cache.find((c) => c.name.slice(2) === "event-votes");
+	const ranks = cache.find((c) => c.name.slice(2) === "trial-ranks");
 
-    owner.send("Someone has joined, copy pasta the following and and welcome them!")
-    owner.send(`Welcome ${member}! As a starter, I suggest you check out our ${event} and ${vote}. If you're into trials, check out ${ranks}. Hope you enjoy your stay here and let us know if you have questions :)`)
+	owner.send(
+		"Someone has joined, copy pasta the following and and welcome them!"
+	);
+	owner.send(
+		`Welcome ${member}! As a starter, have a look at the ${about}, ${event} and ${vote} if you want to participate in our events and voice what events we should do next week. If you're into trials, check out the ${ranks} channel. Hope you'll enjoy your stay here and let us know if you want an invite to the in game guild in case you're not there already.`
+	);
 };
